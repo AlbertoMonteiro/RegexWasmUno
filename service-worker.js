f7d6639fbf5311a957c158eb5a19ad186e73b93a
AlbertoMonteiro <alberto.monteiro@live.com>
2022-02-12 22:06:56 +0000

    Deploying to gh-pages from  @ 224e38eaad8f8750039ac5b2463e3492ad39b5c3 🚀

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,11 @@ let config = {};
 self.addEventListener('install', function (e) {
     console.debug('[ServiceWorker] Installing offline worker');
     e.waitUntil(
-        fetch("./package_0ca4d5f79082759839f2538f02cbac99c24a0b24/uno-config.js")
+        fetch("./package_06e07b28ba13f7f8c761985482b16775a7b14335/uno-config.js")
             .then(r => r.text()
                 .then(configStr => {
                     eval(configStr);
-                    caches.open('package_0ca4d5f79082759839f2538f02cbac99c24a0b24').then(function (cache) {
+                    caches.open('package_06e07b28ba13f7f8c761985482b16775a7b14335').then(function (cache) {
                         console.debug('[ServiceWorker] Caching app binaries and content');
                         return cache.addAll(config.offline_files);
                     });
